78c59cd2ff0a8bf5a8fbc189edd9def571d25f9c
Discordante <diegocerratos@gmail.com>
2024-02-01 19:25:16 +0100

    add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
-  const style = {
-    display: "block",
-    "margin-left": "auto",
-    "margin-right": "auto",
-    "max-width": "100%",
-  };
   return (
-    <iframe
-      width="540"
-      height="305"
-      src="https://ce493616.sibforms.com/serve/MUIFAKmXC3Pla-qQ0OaS-wsYfA0LfSawyORM5NF0SPNOJORIAtfu5bFVdAc_o5B7nvKHxCIIpkihS-f9cXBX6CkcT6soeNq5fcIJIuZyRXYC_bzBmCPvDXR-f8ta8h8CFY93KHoTgsgJC6z8IBU_G6P_LncRebRDNgkM9ZnQhpaOaB_l-CwlE8_6XdDMFtZogFlsDMu8KkEC0qMB"
-      frameborder="0"
-      scrolling="auto"
-      allowfullscreen
-      style={style}
-    ></iframe>
+    <div className="flex justify-center items-center h-screen">
+      <iframe
+        width="100%"
+        height="100%"
+        src="https://ce493616.sibforms.com/serve/MUIFAKmXC3Pla-qQ0OaS-wsYfA0LfSawyORM5NF0SPNOJORIAtfu5bFVdAc_o5B7nvKHxCIIpkihS-f9cXBX6CkcT6soeNq5fcIJIuZyRXYC_bzBmCPvDXR-f8ta8h8CFY93KHoTgsgJC6z8IBU_G6P_LncRebRDNgkM9ZnQhpaOaB_l-CwlE8_6XdDMFtZogFlsDMu8KkEC0qMB"
+      ></iframe>
+    </div>
   );
 }
